c92db0b08ec3d251c86c9d87ce3a34b23194d615
lademir <lademirjunior@hotmail.com>
2022-03-12 02:15:12 -0300

    usando useEffect

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,5 +1,8 @@
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
+import { useState, useEffect } from 'react'
+
+// https://api.github.com/users/lademir/repos
 
 const repository = {
     name: 'unform',
@@ -9,15 +12,24 @@ const repository = {
 
 
 export function RepositoryList() {
+
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/lademir/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, []);
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
 
             <ul>
-               <RepositoryItem repository={repository}/>
-               <RepositoryItem repository={repository}/>
-               <RepositoryItem repository={repository}/>
-               <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
             </ul>
         </section>
     )
