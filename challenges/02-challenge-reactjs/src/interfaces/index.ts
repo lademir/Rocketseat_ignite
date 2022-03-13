c7f479b1da76354908c7056147bdc6a63f0d7f7d
lademir <lademirjunior@hotmail.com>
2022-03-13 01:45:36 -0300

    Finishing second Rocketseat reactJS challenge

---
+++ challenges/02-challenge-reactjs/src/interfaces/index.ts
@@ -0,0 +1,18 @@
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface GenreResponseProps {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }
+
+export type { MovieProps, GenreResponseProps };
